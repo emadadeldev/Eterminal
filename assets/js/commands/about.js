484cbdc0f9a843fff3b6431b219f40425a0d708a
Emad Adel <236038631+emadadeldev@users.noreply.github.com>
2026-05-11 09:21:11 +0300

    Update about.js

--- assets/js/commands/about.js
+++ assets/js/commands/about.js
@@ -1,11 +1,9 @@
 function about(args, { displayOutput }) {
 
     return displayOutput(
-        "Developer: emadadel\n" +
-        "GitHub: https://github.com/emadadeldev\n" +
-        "Eterminal: https://github.com/emadadeldev/Eterminal\n" +
-        "© 2024 - 2026 Emad Adel. All rights reserved.",
-        "info"
+        "Developer Emad Adel\n" +
+        "© Emad Adel. All rights reserved.\n" +
+        "Source Code https://github.com/emadadeldev/Eterminal\n"
     );
 }
 
